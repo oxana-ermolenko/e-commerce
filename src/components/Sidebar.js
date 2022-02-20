c597de331bf6338efa15d2b41a0bc67168f3ede7
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2022-02-19 20:33:14 -0800

    added products_context

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,24 +9,31 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
   return (
     <SidebarContainer>
-      <div className="sidebar-header">
-        <img src={logo} className="logo" alt="comfy sloth" />
-        <button className="close-btn" type="button">
-          <FaTimes />
-        </button>
-      </div>
-      <ul className="links">
-        {links.map(({ id, text, url }) => {
-          return (
-            <li key={id}>
-              <Link to={url}>{text}</Link>
-            </li>
-          );
-        })}
-      </ul>
-      <CartButtons />
+      <aside
+        className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
+      >
+        <div className="sidebar-header">
+          <img src={logo} className="logo" alt="comfy sloth" />
+          <button className="close-btn" type="button" onClick={closeSidebar}>
+            <FaTimes />
+          </button>
+        </div>
+        <ul className="links">
+          {links.map(({ id, text, url }) => {
+            return (
+              <li key={id}>
+                <Link to={url} onClick={closeSidebar}>
+                  {text}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <CartButtons />
+      </aside>
     </SidebarContainer>
   );
 };
