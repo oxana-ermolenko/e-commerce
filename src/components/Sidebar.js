340368996a118e7c0d5dc5c079b017b28ca3da4f
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2022-02-21 13:45:40 -0800

    finished

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,6 +10,7 @@ import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useProductsContext();
+  const { myUser } = useUserContext();
   return (
     <SidebarContainer>
       <aside
@@ -31,6 +32,13 @@ const Sidebar = () => {
               </li>
             );
           })}
+          {myUser && (
+            <li>
+              <Link to="/checkout" onClick={closeSidebar}>
+                checkout
+              </Link>
+            </li>
+          )}
         </ul>
         <CartButtons />
       </aside>
